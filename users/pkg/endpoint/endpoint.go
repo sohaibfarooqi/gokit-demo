7d7aabb47bb70439be728a44df35983fd69bba72
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-06-14 15:01:08 +0800

    updated endpoints definition

--- users/pkg/endpoint/endpoint.go
+++ users/pkg/endpoint/endpoint.go
@@ -6,41 +6,23 @@ import (
 	endpoint "github.com/go-kit/kit/endpoint"
 )
 
-// CreateRequest collects the request parameters for the Create method.
-type CreateRequest struct {
+// UserCreateRequest collects the request parameters for the UserCreate method.
+type UserCreateRequest struct {
 	User service.User
 }
 
-// CreateResponse collects the response parameters for the Create method.
-type CreateResponse struct {
+// UserCreateResponse collects the response parameters for the UserCreate method.
+type UserCreateResponse struct {
 	User service.User `json:"user,omitempty"`
 	Err error `json:"err,omitempty"`
 }
 
-// MakeCreateEndpoint returns an endpoint that invokes Create on the service.
-func MakeCreateEndpoint(s service.UsersService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(CreateRequest)
-		u, err := s.Create(ctx, req.User)
-		return CreateResponse{User: u, Err: err}, nil
-	}
-}
 
-// Failed implements Failer.
-func (r CreateResponse) Failed() error {
-	return r.Err
-}
-
-// Failure is an interface that should be implemented by response types.
-// Response encoders can check if responses are Failer, and if so they've
-// failed, and if so encode them using a separate write path based on the error.
-type Failure interface {
-	Failed() error
-}
+func (r UserCreateResponse) error() error { return r.Err }
 
 // Create implements Service. Primarily useful in a client.
-func (e Endpoints) Create(ctx context.Context, u service.User) (service.User, error) {
-	request := CreateRequest{User: u}
+func (e Endpoints) UserCreate(ctx context.Context, u service.User) (service.User, error) {
+	request := UserCreateRequest{User: u}
 	response, err := e.CreateEndpoint(ctx, request)
 	if err != nil {
 		return service.User{}, nil
@@ -48,3 +30,13 @@ func (e Endpoints) Create(ctx context.Context, u service.User) (service.User, er
 	resp := response.(CreateResponse)
 	return resp.User, resp.Err
 }
+
+// MakeUserCreateEndpoint returns an endpoint that invokes Create on the service.
+func MakeUserCreateEndpoint(s service.UsersService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(UserCreateRequest)
+		u, err := s.UserCreate(ctx, req.User)
+		return UserCreateResponse{User: u, Err: err}, nil
+	}
+}
+
