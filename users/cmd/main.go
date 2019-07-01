44be7ec466bd2408e7c570a870035f1694a36be2
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-07-01 14:28:55 +0800

    user simple flags instead of FlagSet

--- users/cmd/main.go
+++ users/cmd/main.go
@@ -8,19 +8,20 @@ import (
   "os/signal"
   "syscall"
 
+  "github.com/sohaibfarooqi/fragbook/users/pkg"
+  "github.com/go-kit/kit/log"
+
   opentracing "github.com/opentracing/opentracing-go"
   zipkin "github.com/openzipkin/zipkin-go-opentracing"
 
-  "github.com/sohaibfarooqi/fragbook/users/pkg"
-  "github.com/go-kit/kit/log"
 )
-var fs = flag.NewFlagSet("users", flag.ExitOnError)
-var httpAddr = fs.String("http-addr", ":8081", "HTTP listen address")
-var zipkinURL = fs.String("zipkin-url", "http://zipkin:9411/api/v1/spans", "Enable Zipkin tracing via a collector URL e.g. http://localhost:9411/api/v1/spans")
+
+var httpAddr = flag.String("http-addr", ":8081", "HTTP listen address")
+var zipkinURL = flag.String("zipkin", "", "Enable Zipkin tracing via a collector URL e.g. http://localhost:9411/api/v1/spans")
 
 func main(){
 
-  fs.Parse(os.Args)
+  flag.Parse()
 
   // Create a single logger, which we'll use and give to other components.
   var logger log.Logger
@@ -28,7 +29,7 @@ func main(){
   logger = log.With(logger, "ts", log.DefaultTimestampUTC)
   logger = log.With(logger, "caller", log.DefaultCaller)
 
-  logger.Log("tracer", "Zipkin", "URL", *zipkinURL)
+  logger.Log("Zipkin URL", *zipkinURL)
   collector, err := zipkin.NewHTTPCollector(*zipkinURL)
   if err != nil {
     logger.Log("zipkin http collector err", err)
@@ -37,7 +38,11 @@ func main(){
   defer collector.Close()
 
   recorder := zipkin.NewRecorder(collector, false, "http://users:8081", "users")
-  tracer, err := zipkin.NewTracer(recorder, zipkin.ClientServerSameSpan(true), zipkin.TraceID128Bit(true))
+  tracer, err := zipkin.NewTracer(
+    recorder,
+    zipkin.ClientServerSameSpan(true),
+    zipkin.TraceID128Bit(true),
+  )
   if err != nil {
     logger.Log("unable to create zipking tracer", err)
     os.Exit(1)
