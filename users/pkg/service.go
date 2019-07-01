0561c9055eaebeb697781ab02a63b1ca52c79a02
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-07-01 12:50:16 +0800

    zipkin spans inside service

--- users/pkg/service.go
+++ users/pkg/service.go
@@ -4,6 +4,8 @@ import (
   "context"
   "errors"
   "sync"
+
+  opentracing "github.com/opentracing/opentracing-go"
 )
 
 // CRUD interface for users
@@ -31,17 +33,21 @@ type InMemService struct {
 // Inmemory service implementation
 func NewInMemService() UsersService {
   return &InMemService{
-    m: map[string]User{},
+    m: map[string] User{},
   }
 }
 
 func (s *InMemService) Create(ctx context.Context, u User) (User, error) {
+  span := opentracing.SpanFromContext(ctx)
+  defer span.Finish()
   s.mtx.Lock()
   defer s.mtx.Unlock()
   if _, ok := s.m[u.Email]; ok {
+    span.SetTag("error", ErrAlreadyExists)
     return User{}, ErrAlreadyExists // POST = create, don't overwrite
   }
   s.m[u.Email] = u
+  span.SetTag("user", u.Email)
   return u, nil
 }
 
