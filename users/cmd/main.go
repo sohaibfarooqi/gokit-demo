450e34fef2f6625812baa314ed9d60b8a33b3094
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-07-01 12:48:51 +0800

    added config for zipkin

--- users/cmd/main.go
+++ users/cmd/main.go
@@ -8,34 +8,41 @@ import (
   "os/signal"
   "syscall"
 
-  zipkingoopentracing "github.com/openzipkin/zipkin-go-opentracing"
+  opentracing "github.com/opentracing/opentracing-go"
+  zipkin "github.com/openzipkin/zipkin-go-opentracing"
 
   "github.com/sohaibfarooqi/fragbook/users/pkg"
   "github.com/go-kit/kit/log"
 )
-
-var logger log.Logger
-
-var(
-  fs         = flag.NewFlagSet("users", flag.ExitOnError)
-  debugAddr  = fs.String("debug.addr", ":8080", "Debug and metrics listen address")
-  httpAddr   = fs.String("http-addr", ":8081", "HTTP listen address")
-  zipkinURL  = fs.String("zipkin-url", "", "Enable Zipkin tracing via a collector URL e.g. http://localhost:9411/api/v1/spans")
-)
+var fs = flag.NewFlagSet("users", flag.ExitOnError)
+var httpAddr = fs.String("http-addr", ":8081", "HTTP listen address")
+var zipkinURL = fs.String("zipkin-url", "http://zipkin:9411/api/v1/spans", "Enable Zipkin tracing via a collector URL e.g. http://localhost:9411/api/v1/spans")
 
 func main(){
+
+  fs.Parse(os.Args)
+
   // Create a single logger, which we'll use and give to other components.
+  var logger log.Logger
   logger = log.NewLogfmtLogger(os.Stderr)
   logger = log.With(logger, "ts", log.DefaultTimestampUTC)
   logger = log.With(logger, "caller", log.DefaultCaller)
 
   logger.Log("tracer", "Zipkin", "URL", *zipkinURL)
-  collector, err := zipkingoopentracing.NewHTTPCollector(*zipkinURL)
+  collector, err := zipkin.NewHTTPCollector(*zipkinURL)
   if err != nil {
-    logger.Log("err", err)
+    logger.Log("zipkin http collector err", err)
     os.Exit(1)
   }
   defer collector.Close()
+
+  recorder := zipkin.NewRecorder(collector, false, "http://users:8081", "users")
+  tracer, err := zipkin.NewTracer(recorder, zipkin.ClientServerSameSpan(true), zipkin.TraceID128Bit(true))
+  if err != nil {
+    logger.Log("unable to create zipking tracer", err)
+    os.Exit(1)
+  }
+  opentracing.InitGlobalTracer(tracer)
 
   var s pkg.UsersService
   {
@@ -44,7 +51,7 @@ func main(){
 
   var h http.Handler
   {
-    h = pkg.MakeHttpHandler(s, log.With(logger, "component", "HTTP"))
+    h = pkg.MakeHttpHandler(s, log.With(logger, "component", "HTTP"), tracer)
   }
 
   errs := make(chan error)
